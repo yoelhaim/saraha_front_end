7862d0fd6736515b42d8ad5919f78d2ca560489e
yoelhaim <youssefelhaimer8@gmail.com>
2022-06-25 14:27:03 +0100

    add route

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './App.css';
+import Login from './auth/Login';
 
 function App() {
   return (
-    <div className="App">
-     
+    <div className="">
+     <BrowserRouter>
+ 
+     <Routes>
+       <Route path='/login' element={<Login/>} />
+     </Routes>
+     </BrowserRouter>
     </div>
   );
 }
