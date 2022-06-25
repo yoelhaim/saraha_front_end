ca7334863fda14b8d6901068ff030fcf78c364bc
yoelhaim <youssefelhaimer8@gmail.com>
2022-06-25 14:27:20 +0100

    first test css

---
+++ src/auth/Login.js
@@ -0,0 +1,14 @@
+import React from "react";
+function Login() {
+  return(
+   
+    <div className="mt-40">
+     
+     <div className="w-full max-w-md mx-auto  justify-center p-6 bg-white rounded-lg border border-gray-200 shadow-md mt-10">
+       <h1>تسجيل الدخول : </h1>
+     </div>
+    </div>
+  
+  );
+}
+export default Login;
