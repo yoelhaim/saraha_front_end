2f90af54ae62538b2156d4653144f4dc8a24c619
yoelhaim <youssefelhaimer8@gmail.com>
2022-06-28 20:41:09 +0100

    fixed login data

--- src/auth/Login.js
+++ src/auth/Login.js
@@ -1,26 +1,56 @@
 import axios from "axios";
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 
-import { Link } from "react-router-dom";
-function Login() {
+import { Link, useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
+const Login = ({ authenticate }) => {
+  const nav = useNavigate();
+  document.title = "Login";
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const queryString = window.location.search;
+  const urlParams = new URLSearchParams(queryString);
+  const data = urlParams.get("success");
+  if (data) {
+    const decrypt = atob(data);
+    let splitData = decrypt.split("_");
+    console.log(splitData);
+  }
+
   const loginAccount = async (e) => {
     e.preventDefault();
     try {
-      if (email === "") alert("error email");
-      else if (password === "") alert("error password");
+      if (email === "")
+        toast.error("error email is required", global.configTaost);
+      else if (password === "")
+        toast.error("password is required", global.configTaost);
       else {
         const check_login = await axios.post("/auth/login", {
           email: email,
           password: password,
         });
         if (check_login) {
-          console.log(check_login);
+          // eslint-disable-next-line no-lone-blocks
+          {
+            // authenticate();
+
+            toast.success("success login", global.configTaost);
+            localStorage.setItem("token", check_login.data.token);
+            localStorage.setItem("user", JSON.stringify(check_login.data.user));
+            let user = check_login.data.user;
+            global.username = user["userName"];
+            global.userId = user["userId"];
+            global._id = user["_id"];
+            global.email = user["email"];
+            global.fname = user["firstName"];
+            global.lname = user["lastName"];
+            global.token = check_login.data.token;
+            nav("/home");
+          }
         } else console.log("error login");
       }
-    } catch (error) {
-      console.log(error);
+    } catch ({ response }) {
+      toast.warn(response?.data.message, global.configTaost);
     }
   };
   return (
@@ -28,7 +58,7 @@ function Login() {
       <div className="w-full max-w-md mx-auto  justify-center p-6 bg-white rounded-lg border border-gray-200 shadow-md mt-10">
         <h1 className="text-lg">
           <i className="mdi mdi-account-supervisor-circle-outline"></i> تسجيل
-          الدخول{" "}
+          الدخول
         </h1>
         <form onSubmit={loginAccount}>
           <div className="w-full mt-5">
@@ -76,13 +106,16 @@ function Login() {
             </button>
           </div>
           <div className="mt-3 mx-auto  justify-center text-center">
-            <button
-              type="button"
-              className="w-2/1 text-white bg-blue-700 hover:bg-blue-800 focus:outline-none  font-medium rounded-xl text-sm px-5 py-2.5 text-center mr-2 mb-2 "
-            >
-              <i className="mdi mdi-facebook"></i>
-              تسجيل الدخول بفيسبوك
-            </button>
+            <a href="http://localhost:8080/authen">
+              {" "}
+              <button
+                type="button"
+                className="w-2/1 text-white bg-blue-700 hover:bg-blue-800 focus:outline-none  font-medium rounded-xl text-sm px-5 py-2.5 text-center mr-2 mb-2 "
+              >
+                <i className="mdi mdi-facebook"></i>
+                تسجيل الدخول بفيسبوك
+              </button>
+            </a>
           </div>
         </form>
         <p className="mt-3">
@@ -96,5 +129,5 @@ function Login() {
       </div>
     </div>
   );
-}
+};
 export default Login;
