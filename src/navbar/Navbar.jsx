3a5128b2047c8dfad277daf49eaec17bceb64d6c
yoelhaim <youssefelhaimer8@gmail.com>
2022-06-28 20:42:05 +0100

    add navbar

---
+++ src/navbar/Navbar.jsx
@@ -0,0 +1,52 @@
+import { Link } from "react-router-dom";
+
+function Navbar() {
+  return (
+    <nav className="bg-white border-gray-200 px-2 sm:px-4 py-2.5 rounded dark:bg-gray-800">
+      <div class="container flex flex-wrap justify-between items-center mx-auto">
+        <Link to="/home" class="flex items-center">
+          <span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white">
+            Sara7
+          </span>
+        </Link>
+        <div class=" w-full md:block md:w-auto">
+          <ul class="flex flex-col mt-4 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium">
+            <li>
+              <Link
+                to=""
+                className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 "
+              >
+                رسائلي
+              </Link>
+            </li>
+            <li>
+              <Link
+                to=""
+                className="mr-4 block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 "
+              >
+                الإعدادات
+              </Link>
+            </li>
+            <li>
+              <Link
+                to=""
+                className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 "
+              >
+                سياسة الخصوصية
+              </Link>
+            </li>
+            <li>
+              <Link
+                to=""
+                class="block py-2 pr-4 pl-3 text-gray-700 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 "
+              >
+                تسجيل الخروج
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+}
+export default Navbar;
