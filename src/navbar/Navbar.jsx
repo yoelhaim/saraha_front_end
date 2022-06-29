aac5d3c647e4d1c4254ff425a970bcea584c83cf
yoelhaim <youssefelhaimer8@gmail.com>
2022-06-29 14:59:07 +0100

    fixed class in navbar

--- src/navbar/Navbar.jsx
+++ src/navbar/Navbar.jsx
@@ -3,14 +3,14 @@ import { Link } from "react-router-dom";
 function Navbar() {
   return (
     <nav className="bg-white border-gray-200 px-2 sm:px-4 py-2.5 rounded dark:bg-gray-800">
-      <div class="container flex flex-wrap justify-between items-center mx-auto">
-        <Link to="/home" class="flex items-center">
-          <span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white">
+      <div className="container flex flex-wrap justify-between items-center mx-auto">
+        <Link to="/home" className="flex items-center">
+          <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">
             Sara7
           </span>
         </Link>
-        <div class=" w-full md:block md:w-auto">
-          <ul class="flex flex-col mt-4 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium">
+        <div className=" w-full md:block md:w-auto">
+          <ul className="flex flex-col mt-4 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium">
             <li>
               <Link
                 to=""
@@ -38,7 +38,7 @@ function Navbar() {
             <li>
               <Link
                 to=""
-                class="block py-2 pr-4 pl-3 text-gray-700 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 "
+                className="block py-2 pr-4 pl-3 text-gray-700 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 "
               >
                 تسجيل الخروج
               </Link>
