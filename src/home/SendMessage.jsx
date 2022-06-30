068da647d369961546d0d813a991fcaa42784f77
yoelhaim <youssefelhaimer8@gmail.com>
2022-06-30 16:41:40 +0100

    add placeholder

--- src/home/SendMessage.jsx
+++ src/home/SendMessage.jsx
@@ -32,13 +32,13 @@ function SendMessage() {
           htmlFor="message"
           className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400"
         >
-          Your message
+          إكتب رسالتك السرية :
         </label>
         <textarea
           id="message"
           rows="4"
           className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 "
-          placeholder="Leave a comment..."
+          placeholder="إكتب رسالتك السرية هنا"
           onChange={(e) => setMsg(e.target.value)}
         ></textarea>
         <div className="mt-3 mx-auto  justify-center text-center">
