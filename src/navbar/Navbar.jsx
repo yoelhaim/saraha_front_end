38ff26b5e64ec543ba6d7670f99916f33deb81b1
yoelhaim <youssefelhaimer8@gmail.com>
2022-07-02 19:35:50 +0100

    responseve navbar

--- src/navbar/Navbar.jsx
+++ src/navbar/Navbar.jsx
@@ -2,19 +2,19 @@ import { Link } from "react-router-dom";
 
 function Navbar() {
   return (
-    <nav className="bg-gray-300 border-gray-200 px-2 sm:px-4 py-2.5">
-      <div className="container flex flex-wrap justify-between items-center mx-auto">
+    <nav className="bg-gray-300 border-gray-200 px-2  py-2.5">
+      <div className=" flex flex-wrap justify-between items-center ">
         <Link to="/home" className="flex items-center">
           <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">
             الصراحة
           </span>
         </Link>
-        <div className=" w-full md:block md:w-auto">
-          <ul className="flex flex-col mt-4 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium">
+        <div className=" w-full md:block md:w-auto sizefont">
+          <ul className="flex mt-4 flex-row space-x-8 md:mt-0 font-sm">
             <li>
               <Link
                 to=""
-                className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 "
+                className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 "
               >
                 رسائلي
               </Link>
@@ -27,14 +27,7 @@ function Navbar() {
                 الإعدادات
               </Link>
             </li>
-            <li>
-              <Link
-                to=""
-                className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 "
-              >
-                سياسة الخصوصية
-              </Link>
-            </li>
+
             <li>
               <button
                 onClick={global.logOut}
